931851d4928916c197c763fca7da52a3c21d3232
Diya Verma <11diyaverma@gmail.com>
2026-03-21 13:46:45 +0530

    Initial commit for Multi-Commodity Predictor

--- main.py
+++ main.py
@@ -207,4 +207,7 @@ def predict(category):
     
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Render provides a PORT environment variable, we must use it
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port,debug=True)
+    
